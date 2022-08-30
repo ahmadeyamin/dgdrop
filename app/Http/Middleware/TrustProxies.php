41ac867e9878c1365fe71ea85c1c4e24dbdf49a5
Ahmad Eyamin <ahmadeyamin@gmail.com>
2022-08-31 02:39:53 +0600

    ip

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,9 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    protected $proxies = [
+        '10.122.0.2'
+    ];
 
     /**
      * The headers that should be used to detect proxies.
